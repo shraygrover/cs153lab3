66412f7d818ae007aa9bd7762fccdc2d69c0225b
Ng Zhen Teng <ngzhenteng@gmail.com>
2022-03-10 12:15:28 -0800

    Final shm.c
    still only allows a single run of shm_cnt per make qemu-nox

--- xv6/shm.c
+++ xv6/shm.c
@@ -29,12 +29,9 @@ void shminit() {
 }
 
 int shm_open(int id, char **pointer) {
-
-//you write this
     struct proc* curproc = myproc();
     uint sz = PGROUNDUP(curproc->sz);
     int found_page = 0;
-    int opened = 0;
     int i;
     acquire(&(shm_table.lock));
     for (i = 0; i < 64; i++) {
@@ -42,9 +39,8 @@ int shm_open(int id, char **pointer) {
             found_page = 1;
             mappages(curproc->pgdir, (char*)sz, PGSIZE, V2P(shm_table.shm_pages[i].frame), PTE_W | PTE_U);
             shm_table.shm_pages[i].refcnt++;
-            curproc->sz = sz + PGSIZE;
             *pointer = (char *)sz;
-            opened = 1;
+            curproc->sz = sz + PGSIZE;
             break;
         }
     }
@@ -57,27 +53,19 @@ int shm_open(int id, char **pointer) {
                 memset(shm_table.shm_pages[j].frame, 0, PGSIZE);
                 mappages(curproc->pgdir, (char*)sz, PGSIZE, V2P(shm_table.shm_pages[j].frame), PTE_W | PTE_U);
                 shm_table.shm_pages[j].refcnt++;
-                curproc->sz = sz + PGSIZE;
                 *pointer = (char *)sz;
-                opened = 1;
+                curproc->sz = sz + PGSIZE;
                 break;
             }
         }
     }
     release(&(shm_table.lock));
-    if (opened == 1) {
-        return 0;
-    } else {
-        return 1;
-    }
-//    return 0; //added to remove compiler warning -- you should decide what to return
+    return 0; //added to remove compiler warning -- you should decide what to return
 }
 
 
 int shm_close(int id) {
-//you write this too!
     int closed = 0;
-//    struct proc* curproc = myproc();
     int i;
     acquire(&(shm_table.lock));
     for (i = 0; i < 64; i++) {
@@ -87,17 +75,13 @@ int shm_close(int id) {
             }
             closed = 1;
             if (shm_table.shm_pages[i].refcnt == 0) {
-                cprintf("Cleared the shm entry");
                 shm_table.shm_pages[i].id = 0;
-//                kfree(shm_table.shm_pages[i].frame);
                 shm_table.shm_pages[i].frame = 0;
-//                mappages(curproc->pgdir, )
-                break;
             }
+            break;
         }
     }
     release(&(shm_table.lock));
-    cprintf("Closed value: %d", closed);
     if (closed == 1) {
         return 0;
     } else {
